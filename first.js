ac4e9cb498270bb36f6edd1f0458c86696f96ff0
SaipavanChittiprolus <132157406+SaipavanChittiprolus@users.noreply.github.com>
2024-03-09 20:18:11 +0530

    changes

--- first.js
+++ first.js
@@ -1,1 +1,20 @@
 console.log("Let's Acheive the heights bae")
+/* learning these type of content is very interesting compared to other youtube videos this feels very new 
+*first i learned about the varible like 
+1.const
+2.let 
+* then the data types 
+1.number
+2.string
+3.bigint 
+4.boolean
+5.symbol
+6.undefined 
+7.null //object 
+* type converstions 
+1.Numbers 
+there are some drawbacks in js to convert the inputs 
+they convert everything without checking them  
+if the 1 is converted it is true // if 0 it is false 
+if string is empty it is false // if it has content it is true 
+*/
